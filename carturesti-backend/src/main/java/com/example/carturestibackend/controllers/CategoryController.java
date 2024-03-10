4d7c9bd9556cd2f2c3b617c120e9154002579191
MatisOana6 <matis.oana@yahoo.com>
2024-03-10 04:13:00 +0200

    crud order,category controller

--- carturesti-backend/src/main/java/com/example/carturestibackend/controllers/CategoryController.java
+++ carturesti-backend/src/main/java/com/example/carturestibackend/controllers/CategoryController.java
@@ -1,4 +1,54 @@
 package com.example.carturestibackend.controllers;
 
+import com.example.carturestibackend.services.CategoryService;
+import com.example.carturestibackend.dtos.CategoryDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/category")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<CategoryDTO>> getCategories() {
+        List<CategoryDTO> dtos = categoryService.findCategories();
+        return new ResponseEntity<>(dtos, HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Long> insertCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
+        long categoryID = categoryService.insert(categoryDTO);
+        return new ResponseEntity<>(categoryID, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{id_category}")
+    public ResponseEntity<CategoryDTO> getCategory(@PathVariable("id_category") long categoryID) {
+        CategoryDTO dto = categoryService.findCategoryById(categoryID);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id_category}")
+    public ResponseEntity<String> deleteCategory(@PathVariable("id_category") long categoryID) {
+        categoryService.deleteCategoryById(categoryID);
+        return new ResponseEntity<>("Category with ID " + categoryID + " deleted successfully", HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id_category}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable("id_category") long categoryID, @Valid @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO updatedCategory = categoryService.updateCategory(categoryID, categoryDTO);
+        return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
+    }
 }
--- carturesti-backend/src/main/java/com/example/carturestibackend/controllers/OrderController.java
+++ carturesti-backend/src/main/java/com/example/carturestibackend/controllers/OrderController.java
@@ -1,4 +1,54 @@
 package com.example.carturestibackend.controllers;
 
+import com.example.carturestibackend.services.OrderService;
+import com.example.carturestibackend.dtos.OrderDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/order")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<OrderDTO>> getOrders() {
+        List<OrderDTO> dtos = orderService.findOrders();
+        return new ResponseEntity<>(dtos, HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Long> insertOrder(@Valid @RequestBody OrderDTO orderDTO) {
+        long orderID = orderService.insert(orderDTO);
+        return new ResponseEntity<>(orderID, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{id_order}")
+    public ResponseEntity<OrderDTO> getOrder(@PathVariable("id_order") long orderID) {
+        OrderDTO dto = orderService.findOrderById(orderID);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id_order}")
+    public ResponseEntity<String> deleteOrder(@PathVariable("id_order") long orderID) {
+        orderService.deleteOrderById(orderID);
+        return new ResponseEntity<>("Order with ID " + orderID + " deleted successfully", HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id_order}")
+    public ResponseEntity<OrderDTO> updateOrder(@PathVariable("id_order") long orderID, @Valid @RequestBody OrderDTO orderDTO) {
+        OrderDTO updatedOrder = orderService.updateOrder(orderID, orderDTO);
+        return new ResponseEntity<>(updatedOrder, HttpStatus.OK);
+    }
 }
